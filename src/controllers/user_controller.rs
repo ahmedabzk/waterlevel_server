45535f448d54b5fa2427d093b749322040f1675e
Yasir Shariff <goodwonder5@gmail.com>
2023-04-02 10:19:58 +0300

    Add comments for fixes

--- src/controllers/user_controller.rs
+++ src/controllers/user_controller.rs
@@ -1,31 +1,29 @@
-use serde_json;
-use serde::{
-    Serialize,
-    Deserialize,
-};
 use axum::{
-    StatusCode,
-    Extension, Json,
+    Extension,
+    Json,
+    StatusCode, // -> Wrong import, this enum should be found in axum::http::StatusCode
 };
+use serde::{Deserialize, Serialize};
+use serde_json;
 
 use sqlx::PgPool;
 use validators::serde_json::json;
 
-mod models;
+mod models; //-> You are defining the module vibaya, this means that this module is found in the controller folder, which is wrong
 
-use models::users::User;
-
+use models::users::User; // -> Fix above error, you should be able to fix this
 
 pub async fn register(
-Json(credentials): Json<users::User>,
-Extension(pool): Extension<PgPool>
-) -> Result<Json<Value>, StatusCode>{
+    Json(credentials): Json<users::User>,
+    Extension(pool): Extension<PgPool>,
+) -> Result<Json<Value>, StatusCode> {
     // check if the fields are empty strings
-    if credentials.first_name.is_empty() 
-    || credentials.last_name.is_empty() 
-    || credentials.email.is_empty() 
-    || credentials.password.is_empty(){
-        return Err(StatusCode)
+    if credentials.first_name.is_empty()
+        || credentials.last_name.is_empty()
+        || credentials.email.is_empty()
+        || credentials.password.is_empty()
+    {
+        return Err(StatusCode); // -> This doesn't make sense, StatusCode is an enum, return one of the variants
     }
 
     // get the user of the email from the database
@@ -37,30 +35,34 @@ Extension(pool): Extension<PgPool>
     .fetch_optional(&pool)
     .await
     .map_err(|err| {
+        // -> Bad error handling
         dbg!(err);
-        StatusCode::InternalServerError;
+        StatusCode::InternalServerError; // -> Re-read the docs of the StatusCode variants
     });
 
     // if user already exits send user already exits
     if let Some(_) = user {
-        return Err(StatusCode::BadRequest)
+        // -> You can't do this
+        return Err(StatusCode::BadRequest);
     }
 
-    let result = sqlx::query("INSERT into users (first_name, last_name, email, password) values ($1, %2, $3, $4)")
-        .bind(&credentials.first_name)
-        .bind(credentials.last_name)
-        .bind(credentials.email)
-        .bind(credentials.password)
-        .execute(&pool)
-        .await
-    .map_err(|err|{
+    let result = sqlx::query(
+        "INSERT into users (first_name, last_name, email, password) values ($1, %2, $3, $4)",
+    )
+    .bind(&credentials.first_name)
+    .bind(credentials.last_name)
+    .bind(credentials.email)
+    .bind(credentials.password)
+    .execute(&pool)
+    .await
+    .map_err(|err| {
         dbg!(err);
-        return Err(StatusCode::InternalServerError)
-    })?;
+        return Err(StatusCode::InternalServerError);
+    })?; // -> This won't work
 
-    if result.rows_affected() < 1{
+    if result.rows_affected() < 1 {
         Err(StatusCode::InternalServerError)
-    }else {
+    } else {
         Ok(Json(json!({"msg": "registered successfuly"})))
     }
 }
--- src/middlewares/mod.rs
+++ src/middlewares/mod.rs
@@ -1,1 +1,3 @@
-pub mod middlewares;
+pub mod middlewares; //-> Wrong use of this, this means you have a module called middlewares.rs, while infact you don't have it.
+
+// I will suggest you take a look at the folder/file structure from the rust book, and start a new small practise project , and there are two ways to define modules in rust, the one you are using now and there's another one, make sure you take a look both of em
