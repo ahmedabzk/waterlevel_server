bc4705f115e04c77b20712ba8f933cc3b4e849d7
ahmedabzk <ahmed.abzk@gmail.com>
2023-04-08 01:36:34 +0300

    waterlevel

--- src/controllers/user_controller.rs
+++ src/controllers/user_controller.rs
@@ -1,17 +1,12 @@
-use axum::{
-    Extension,
-    Json,
-};
+use axum::{Extension, Json};
 // use serde::{Deserialize, Serialize};
 // use serde_json;
 
 use sqlx::PgPool;
 use validators::serde_json::{json, Value};
 
-//-> You are defining the module vibaya, this means that this module is found in the controller folder, which is wrong
-
-use crate::models::users::User;
-use crate::errors::custom_errors::CustomErrors; // -> Fix above error, you should be able to fix this
+use crate::errors::custom_errors::CustomErrors;
+use crate::models::users::{User};
 
 pub async fn register(
     Extension(pool): Extension<PgPool>,
@@ -22,40 +17,40 @@ pub async fn register(
         || credentials.last_name.is_empty()
         || credentials.email.is_empty()
         || credentials.password.is_empty()
+
     {
-        return Err(CustomErrors::MissingCredential); // -> This doesn't make sense, StatusCode is an enum, return one of the variants
+        return Err(CustomErrors::MissingCredential);
     }
 
     // get the user of the email from the database
 
-    let user = sqlx::query_as::<_, User>(
-        "SELECT email, password FROM users where email = $1",
-    )
-    .bind(&credentials.email)
-    .fetch_optional(&pool)
-    .await
-    .map_err(|_err| {
-        CustomErrors::InternalServerError; // -> Re-read the docs of the StatusCode variants
-    });
+    let user = sqlx::query("SELECT email, password FROM users where email = $1")
+        .bind(&credentials.email)
+        .fetch_optional(&pool)
+        .await
+        .map_err(|err| {
+            dbg!(err);
+            CustomErrors::InternalServerError
+        });
 
     // if user already exits send user already exits
-    if let Some(_) = user {
-        // -> You can't do this
+    if let Ok(_) = user {
         return Err(CustomErrors::UserAlreadyExist);
     }
 
     let result = sqlx::query(
-        "INSERT into users (first_name, last_name, email, password) values ($1, %2, $3, $4)",
+        "INSERT into users (id, first_name, last_name, email, password) values ($1, %2, $3, $4, $5)",
     )
-    .bind(&credentials.first_name)
+    .bind(credentials.id)
+    .bind(credentials.first_name)
     .bind(credentials.last_name)
     .bind(credentials.email)
     .bind(credentials.password)
+    // .bind(credentials.create_at)
+    // .bind(credentials.updated_at)
     .execute(&pool)
     .await
-    .map_err(|_err| {
-        CustomErrors::InternalServerError
-    })?; // -> This won't work
+    .map_err(|_err| CustomErrors::InternalServerError)?;
 
     if result.rows_affected() < 1 {
         Err(CustomErrors::InternalServerError)
--- src/models/users.rs
+++ src/models/users.rs
@@ -1,12 +1,22 @@
 
-use serde::{Serialize, Deserialize};
-use sqlx;
+use chrono;
+use serde::{Deserialize, Serialize};
+use sqlx::{
+    FromRow
+};
 
-#[derive(Serialize, sqlx::FromRow, Deserialize)]
-pub struct User{
+use uuid::Uuid;
+
+#[derive(Debug,Serialize,FromRow,Deserialize)]
+pub struct User {
+    pub id: Uuid::new_v4(),
     pub first_name: String,
     pub last_name: String,
     pub email: String,
-    pub password: String
+    pub password: String,
+    pub create_at: Option<chrono::DateTime<chrono::Utc>>,
+    pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
 }
+
+let id = Uuid::new_v4();
 
